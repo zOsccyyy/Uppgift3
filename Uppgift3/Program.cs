1bd44fa62b86a4eda24dc597d83467bcc0d92173
Admin <Admin@TEB123008>
2023-09-08 15:19:42 +0200

    Add project files.

---
+++ Uppgift3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace Uppgift_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         /\\         ");
+            Console.WriteLine("        /  \\        ");
+            Console.WriteLine("       /    \\       ");
+           Console.WriteLine("      /______\\      ");
+        }
+    }
+}
